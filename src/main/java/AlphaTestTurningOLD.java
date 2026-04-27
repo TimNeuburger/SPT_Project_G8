1be89f0a2a2c012378cf96e70137929774c563b6
Tim Neuburger <neuburger.tim@gmail.com>
2026-04-27 15:35:28 +0200

    Alphatest preparation

---
+++ src/main/java/AlphaTestTurningOLD.java
@@ -0,0 +1,46 @@
+import Components.*;
+import ev3dev.sensors.EV3Key;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.robotics.Color;
+
+public class AlphaTestTurning {
+
+    public static void main(final String[] args){
+
+        System.out.println("Creating Motor A & B");
+        MotorEV3 leftMotor = new MotorEV3(MotorPort.A);
+        MotorEV3 rightMotor = new MotorEV3(MotorPort.B);
+        IRSensor irSensor = new IRSensor(SensorPort.S3);
+        DifferentialDrive motors = new DifferentialDrive(leftMotor, rightMotor);
+        EV3Key Enter = new EV3Key(28);
+
+        System.out.println("Creating Sensors");
+        ColorSensorEV3 leftColor = new ColorSensorEV3(SensorPort.S1, ColorMode.RED);
+        ColorSensorEV3 rightColor = new ColorSensorEV3(SensorPort.S2, ColorMode.RED);
+
+        //To Stop the motor in case of pkill java for example
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                System.out.println("Emergency Stop");
+                motors.stop();
+            }
+        }));
+
+        System.out.println("Start program");
+        while(true) {
+
+            Enter.waitForPress();
+            leftColor.setLight(Color.WHITE);
+            rightColor.setLight(Color.WHITE);
+            motors.move(600,600);
+            while (irSensor.getDistance() < 50) {
+                if(leftColor.getValue1() < 300) {
+                    motors.move(600, -600);
+                    System.
+                }
+                if
+            }
+        }
+    }
+}
